6eae626b33f8410db1b49b2cc6f9319093185467
zhanglongtai <neo_seele@hotmail.com>
2018-02-08 17:28:32 +0800

    config file

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+var webpack = require('webpack')
+
+module.exports = {
+    entry: {
+        mainView: './src/mainViewIndex.js',
+    },
+  
+    output: {
+        path: `${__dirname}/dist/js`,
+        filename: '[name].index.js',
+    },
+
+    module: {
+        loaders: [
+            { 
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader',
+                query:
+                    {
+                        presets:['env', 'react'],
+                    },
+            },
+        ],
+    },
+}
